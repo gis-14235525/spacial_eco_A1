6fd2d0678a06bdd9922985245ff71f6b20b20b41
gis-14235525 <m3gam3kin@outlook.com>
2026-03-14 18:00:20 +0000

    charactistic analysis(changed)

--- meles_A1.R
+++ meles_A1.R
@@ -90,13 +90,27 @@ plot(melesFin$geometry, add = TRUE)
 
 # characteristic scale
 
-# read in our LSM raster data
-LCM_scale <- rast("LCMUK.tif")
+# use previous badger points, but transform back to EPSG:4326
+meles.scale <- st_transform(meles.sp, 4326)
 
-# get coordinates of badger points
-melesCoords <- st_coordinates(meles.sp)
+# convert to terra vector 
+meles.scale <- vect(meles.scale)
+
+#First set the exetent to something workable
+studyExtent<-c(-4.2,-2.7,56.5,57.5) #list coordinates in the order: min x, max x, min y, max y
+
+#now crop our points to this area
+C<-crop(meles.scale,studyExtent)
+
+# read in LSM raster data
+LCM_scale = rast("LCMUK.tif")
+
+# create an object containing the crs information for LCM inside the project
+melesFin<-project(C,crs(LCM_scale))
 
 # crop the land cover data to the extent of our points data (plus 5km to allow space for the buffers we will create in subsequent steps)
+melesCoords<-crds(melesFin)
+
 x.min <- min(melesCoords[,1]) - 5000
 x.max <- max(melesCoords[,1]) + 5000
 y.min <- min(melesCoords[,2]) - 5000
@@ -107,4 +121,4 @@ extent.new <- ext(x.min, x.max, y.min, y.max)
 LCM_scale <- crop(LCM_scale$LCMUK_1, extent.new)
 
 plot(LCM_scale)
-plot(meles.sp,add = TRUE)
+plot(melesFin,add = TRUE)
